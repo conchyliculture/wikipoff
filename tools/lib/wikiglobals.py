968e60fac19a64c74157738000a1a85a7baba5cd
renzokuken <pocketwiki-github@renzokuken.eu>
2014-03-04 14:38:10 +0100

    new file storing global values

---
+++ tools/lib/wikiglobals.py
@@ -0,0 +1,48 @@
+#lol
+
+##
+# Whether to preseve links in output
+#
+keepLinks = True
+
+# handle 'a' separetely, depending on keepLinks
+ignoredTags = [
+         'big', 'blockquote', 'center', 'cite', 'div', 'em',
+        'font', 'h1', 'h2', 'h3', 'h4', 'hiero',  'kbd', 'nowiki',
+        's', 'tt', 'var',
+]
+
+# This is obtained from the dump itself
+prefix = None # Lost?
+
+##
+# Whether to transform sections into HTML
+#
+keepSections = True
+
+##
+# Recognize only these namespaces
+# w: Internal links to the Wikipedia
+# wiktionary: Wiki dictionry
+# wikt: shortcut for Wikctionry
+#
+acceptedNamespaces = set(['w', 'wiktionary', 'wikt'])
+
+##
+# Drop these elements from article text
+#
+discardElements = set([
+        'gallery', 'timeline', 'noinclude', 'pre',
+        'table', 'tr', 'td', 'th', 'caption',
+        'form', 'input', 'select', 'option', 'textarea',
+        'ul', 'li', 'ol', 'dl', 'dt', 'dd', 'menu', 'dir',
+        'ref', 'references', 'img', 'imagemap', 'source'
+        ])
+
+selfClosingTags = [ 'br', 'hr', 'nobr', 'ref', 'references' ]
+
+lang=None
+
+placeholder_tags = { 'code':'codice'}
+
+convert=True
