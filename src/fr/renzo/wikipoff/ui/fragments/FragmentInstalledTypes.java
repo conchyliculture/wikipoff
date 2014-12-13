3eef562999f1c3dea40f62316b89d2fc1c07c19f
renzo <pocketwiki-github@renzokuken.eu>
2014-12-13 23:02:32 +0100

    no need to call a layout, also moved stuff to onactivitycreated instead of oncreateview

--- src/fr/renzo/wikipoff/ui/fragments/FragmentInstalledTypes.java
+++ src/fr/renzo/wikipoff/ui/fragments/FragmentInstalledTypes.java
@@ -3,57 +3,35 @@ package fr.renzo.wikipoff.ui.fragments;
 import java.util.ArrayList;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
-import fr.renzo.wikipoff.R;
 import fr.renzo.wikipoff.ui.activities.WikiManagerActivity;
 
 public class FragmentInstalledTypes extends SherlockListFragment {
 
+	@SuppressWarnings("unused")
 	private static final String TAG = "FragmentInstalledTypes";
-	private View wholeview;
 	private WikiManagerActivity manageractivity;
 	private ArrayList<String> wikitypes;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	  @Override
+      public void onActivityCreated(Bundle savedInstanceState) {
+          super.onActivityCreated(savedInstanceState);
+          manageractivity = (WikiManagerActivity) getSherlockActivity();
+  		this.wikitypes = manageractivity.getWikiTypes();
 
-		manageractivity = (WikiManagerActivity) getSherlockActivity();
-		this.wikitypes = manageractivity.getWikiTypes();
-		wholeview=inflater.inflate(R.layout.fragment_wiki_manager_installed,container, false);
-
-		setListAdapter(new ArrayAdapter<String>(manageractivity,
-				android.R.layout.simple_list_item_1,
-				this.wikitypes)
-				);
-		
-		return wholeview;
-	}
-
-//	// The following code shows how to properly open new fragment. It assumes
-//	// that parent fragment calls its activity via interface. This approach
-//	// is described in Android development guidelines.
-//	@Override
-//	public void onItemSelected(String item)
-//	{
-//		ItemFragment fragment = new ItemFragment();
-//		Bundle args = new Bundle();
-//		args.putString("item", item);
-//		fragment.setArguments(args);
-//		addFragment(fragment);
-//	}
+  		setListAdapter(new ArrayAdapter<String>(manageractivity,
+  				android.R.layout.simple_list_item_1,
+  				this.wikitypes)
+  				);
+      }
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		Log.d(TAG,"Clicked on "+position+" type:"+this.wikitypes.get(position));
 		FragmentInstalledWikis fragment = new FragmentInstalledWikis();
 		Bundle args = new Bundle();
 		args.putString("type", this.wikitypes.get(position));
